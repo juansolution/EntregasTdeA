511e73b3d0b3aa19b654c152d9b2593d5b136746
Juan Esteban Piedrahita <juan.piedrahita@satrack.com>
2019-06-04 20:27:10 -0500

    subiendo archivos del proyecto

---
+++ src/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const bodyparser = require("body-parser");
+
+require("./helpers/helpers");
+//const exphbs = require('express-handlebars');
+
+const dirpublic = path.join(__dirname,"../public");
+//const dirviews = path.join(__dirname,"./views");
+const dirmodules = path.join(__dirname,"../node_modules");
+const dirpartials = path.join(__dirname,"./partials")
+
+app.use(express.static(dirpublic));
+app.use('/css', express.static(dirmodules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirmodules + '/jquery/dist'));
+app.use('/js', express.static(dirmodules + '/popper.js/dist'));
+app.use('/js', express.static(dirmodules + '/bootstrap/dist/js'));
+app.use(bodyparser.urlencoded({extended:false}));
+
+
+/* se llama al engine view y se le settea que va ser hbs  */
+
+app.set('view engine', 'hbs');
+
+/* Registre las partials*/
+console.log(dirpartials);
+hbs.registerPartials(dirpartials);
+
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        nusuario : "J Piedrahita"
+    });
+});
+
+app.get('/testpartials',(req,res)=>{
+    res.render('testpartial');
+});
+
+app.post('/promedios',(req,res)=>{
+    res.render('promedio',{
+        nombreestudiante: req.body.nombre,
+        nota1: parseInt(req.body.nota1),
+        nota2: parseInt(req.body.nota2),
+        nota3: parseInt(req.body.nota3)
+
+    });
+});
+
+app.get('/testform',(req,res)=>{
+    res.render('testform');
+});
+
+app.get('*',(req,res)=>{ 
+    res.render('errorPage');
+
+ });
+
+app.listen(3001,()=>{console.log("Aplicacion iniciada por el puerto 3001...")});
